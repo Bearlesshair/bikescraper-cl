11780dd8fa3ddece56b0ccb127feef232f35e189
Nick Florentius <donutpop365@gmail.com>
2019-07-14 23:59:41 -0500

    Add Gmail sender functionality. Option to send copy of spreadsheet via email when new listings found.

--- bikescraper-cl.py
+++ bikescraper-cl.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 import argparse
-import config, query, excel
+import config, query, excel, sender
 
 parser = argparse.ArgumentParser(prog='bikescraper-cl', description='Craigslist bicycle finder')
 parser.add_argument('--config', help='Config file location (default .bikescraper-cl/config.yaml)')
@@ -14,3 +14,8 @@ storage = query.do(configDict)
 
 # export to excel file if new relevant listings
 excelfile = excel.export(storage)
+
+# if excelfile is not empty and gmail option enabled, send spreadsheet
+if configDict['sendemail'] is True and excelfile is not None:
+    print("Emailing spreadsheet to %s" % configDict['toemail'])
+    sender.send(excelfile, configDict['botuser'], configDict['botpassword'], configDict['toemail'])
